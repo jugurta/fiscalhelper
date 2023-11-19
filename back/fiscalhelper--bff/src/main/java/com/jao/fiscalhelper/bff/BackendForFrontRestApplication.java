3eeeeba8b3975fcc61c888d83a22a7923dcf9fad
Jugurtha AIT OUFELLA <jugurtha.aitoufella@ext.leroymerlin.fr>
2023-11-19 15:34:39 +0100

    chore(hooks): adding new conventional commit check hook

--- back/fiscalhelper--bff/src/main/java/com/jao/fiscalhelper/bff/BackendForFrontRestApplication.java
+++ back/fiscalhelper--bff/src/main/java/com/jao/fiscalhelper/bff/BackendForFrontRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendForFrontRestApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BackendForFrontRestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForFrontRestApplication.class, args);
+	}
 }
